6c97a660011fbfcba5bd85db1530d872467ffcec
davips <dpsabc@gmail.com>
2020-01-04 07:21:04 -0300

    blosc is buggy, usando pack_data now / fastdatamonkeypatch

--- tatu/compression.py
+++ tatu/compression.py
@@ -1,6 +1,33 @@
 import _pickle as pickle
 import lz4.frame as lz
 
+import zstd as zs
+# from numpy import ndarray
+import numpy
+
+
+def pack_data(obj):
+    # pickled = pickle.dumps(obj) # 1169_airlines explodes here with RAM < ?
+    if isinstance(obj, numpy.ndarray):
+        h, w = obj.shape
+        fast_reduced = lz.compress(obj.reshape(w * h), compression_level=1)
+    else:
+        pickled = pickle.dumps(obj)
+        fast_reduced = lz.compress(b'Data'+pickled, compression_level=1)
+    return zs.compress(fast_reduced)
+
+
+def unpack_data(dump, w=None, h=None):
+    decompressed = zs.decompress(dump)
+    fast_decompressed = lz.decompress(decompressed)
+    if fast_decompressed[:4] == b'Data':
+        return pickle.loads(fast_decompressed[4:])
+    else:
+        return numpy.reshape(numpy.frombuffer(fast_decompressed), newshape=(h, w))
+
+    # 1169_airlines explodes here with RAM < ?
+    # return pickle.loads(fast_decompressed)
+
 
 def pack_object(obj):
     """
@@ -21,4 +48,3 @@ def unpack_object(dump):
     decompressed = blosc.decompress(dump)
     fast_decompressed = lz.decompress(decompressed)
     return pickle.loads(fast_decompressed)
-
--- tatu/file.py
+++ tatu/file.py
@@ -1,16 +1,19 @@
-from cururu.compression import pack_object, unpack_object
+from cururu.compression import pack_object, unpack_object, pack_data, \
+    unpack_data
 
 
 def save(filename, obj):
     """Compress and save a python object (Data, Transformer) as a file."""
     f = open(filename, 'wb')
-    f.write(pack_object(obj))
+    # f.write(pack_object(obj))  # blosc is buggy
+    f.write(pack_data(obj))
     f.close()
 
 
 def load(filename):
     """Load a compressed python object (Data, Transformer) from file."""
     f = open(filename, 'rb')
-    res = unpack_object(f.read())
+    # res = unpack_object(f.read())  # blosc is buggy
+    res = unpack_data(f.read())
     f.close()
     return res
