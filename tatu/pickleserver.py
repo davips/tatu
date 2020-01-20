1682dbdbf8bcaeea681556546e6ebdaadd1ca87f
davips <dpsabc@gmail.com>
2020-01-20 20:24:38 -0300

    reverted PickleSErver

--- tatu/pickleserver.py
+++ tatu/pickleserver.py
@@ -1,11 +1,13 @@
-import _pickle as pickle
 import os
-from glob import glob
-from pathlib import Path
+import traceback
 
+from cururu.compression import unpack_object, pack_object
 from cururu.disk import save, load
 from cururu.persistence import Persistence, LockedEntryException, \
     FailedEntryException, DuplicateEntryException, UnlockedEntryException
+import _pickle as pickle
+from pathlib import Path
+from glob import glob
 
 
 class PickleServer(Persistence):
@@ -40,18 +42,28 @@ class PickleServer(Persistence):
 
         return transformed_data
 
-    def store(self, data, fields=None, check_dup=True):
+    def store(self, data, previous_data=None, transformations=None, fields=None,
+              check_dup=True):
         """The dataset name of data_out will be the filename prefix for
         convenience."""
         # TODO: deal with fields and missing fields?
+        if bool(previous_data) != bool(transformations):
+            raise Exception('It is not possible to store data with '
+                            'previous_data, but without transformation - '
+                            'and vice-versa!')
+        if previous_data is None:
+            previous_data = data
+        if fields is None:
+            fields = ['X', 'Y']
 
-        filename = self._filename(data.dataset.name, data)
+        filename = self._filename(data.dataset.name,
+                                  previous_data, transformations)
 
         # Already exists?
         if check_dup and Path(filename).exists():
             raise DuplicateEntryException('Already exists:', filename)
 
-        locked = self._filename('', data)
+        locked = self._filename('', previous_data, transformations)
         if Path(locked).exists():
             os.remove(locked)
 
@@ -66,36 +78,24 @@ class PickleServer(Persistence):
                 datas.append(data.phantom)
         return datas
 
-    def unlock(self, data, transformations):
-        print('qqqqqqqqqqqqq unlocking', data, transformations)
-        filename = self._filename('*', data, transformations)
-        if not Path(filename).exists():
-            raise UnlockedEntryException
-        os.remove(filename)
-
-    def _filename(self, prefix, data, transformations=None):
-        # TODO: move NoThing uuids to parent (Persistence)
-        if transformations is None:
-            transformations = []
-
-        transf_sids = []
-        for transf in data.history.transformations + transformations:
-            # Using sid since linux file names are limited to 255 characters.
-            transf_sids.append(transf.sid)
-
-        path = self.db + '/'
-        rest = '-' + data.dataset.uuid + '-' + ','.join(transf_sids) + '.dump'
+    def _filename(self, prefix, data, transformation):
+        # TODO: move NoThing uuids to parent
+        prev_uuid = 'DØØØØØØØØØØØØØØØØØØ0' if data is None \
+            else data.uuid
+        transf_uuid = 'TØØØØØØØØØØØØØØØØØØ0' if transformation is None \
+            else transformation.uuid
+        rest = '-' + prev_uuid + '-' + transf_uuid + '.dump'
         if prefix == '*':
-            query = path + '*' + rest
+            query = self.db + '/*' + rest
             lst = glob(query)
             if len(lst) > 1:
                 raise Exception('Multiple files found:', query, lst)
             if len(lst) == 1:
                 return lst[0]
             else:
-                return path + data.dataset.name + rest
+                return self.db + '/' + rest
         else:
-            return path + prefix + rest
+            return self.db + '/' + prefix + rest
 
     def _load(self, filename):
         """
@@ -103,13 +103,18 @@ class PickleServer(Persistence):
         :param filename: file dataset
         :return: Data
         """
-        if self.speed:
-            f = open(filename, 'rb')
-            res = pickle.load(f)
-            f.close()
-            return res
-        else:
-            return load(filename)
+        try:
+            if self.speed:
+                f = open(filename, 'rb')
+                res = pickle.load(f)
+                f.close()
+                return res
+            else:
+                return load(filename)
+        except Exception as e:
+            traceback.print_exc()
+            print('Problems loading', filename)
+            exit(0)
 
     def _dump(self, data, filename):
         """
@@ -140,3 +145,8 @@ class PickleServer(Persistence):
     #             matrices.append(arg)
     #     for mat in matrices:
     #         del data.__dict__[mat]
+    def unlock(self, previous_data, transformation):
+        filename = self._filename('*', previous_data, transformation)
+        if not Path(filename).exists():
+            raise UnlockedEntryException
+        os.remove(filename)
