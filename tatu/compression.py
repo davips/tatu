66f843973625290331388762df90e6d2116dcd2a
davips <dpsabc@gmail.com>
2020-01-23 21:56:10 -0300

    compression now accepts any python object; np float64 avoids pickle

--- tatu/compression.py
+++ tatu/compression.py
@@ -1,41 +1,36 @@
 import _pickle as pickle
-import lz4.frame as lz
 
+import lz4.frame as lz
+import numpy as np
 import zstandard as zs
+
+
 # TODO: make a permanent representative dictionary and check if it
 #  reduces compression time and size of textual info like transformations:
 #  dict_data = zs.train_dictionary(131072, samples)
 #  cctx = zs.ZstdCompressor(dict_data=dict_data)
 #  cctx.compress(fast_reduced)
 
-# TODO: check if multi-threaded zstandard lib is really faster than zstd lib
-#  for matrices/text.
-import numpy
-
 
 def pack_data(obj):
-    # pickled = pickle.dumps(obj) # 1169_airlines explodes here with RAM < ?
-    if isinstance(obj, numpy.ndarray):
+    if isinstance(obj, np.ndarray) and str(obj.dtype) == 'float64':
         h, w = obj.shape
         fast_reduced = lz.compress(obj.reshape(w * h), compression_level=1)
     else:
-        pickled = pickle.dumps(obj)
-        fast_reduced = lz.compress(b'Obj'+pickled, compression_level=1)
+        pickled = pickle.dumps(obj)  # 1169_airlines explodes here with RAM < ?
+        fast_reduced = lz.compress(b'Obj' + pickled, compression_level=1)
     cctx = zs.ZstdCompressor(threads=-1)
     return cctx.compress(fast_reduced)
 
 
 def unpack_data(dump, w=None, h=None):
-    # cctx = zs.ZstdDecompressor()
-    decompressed = zs.decompress(dump)
+    cctx = zs.ZstdDecompressor()
+    decompressed = cctx.decompress(dump)
     fast_decompressed = lz.decompress(decompressed)
     if fast_decompressed[:3] == b'Obj':
-        return pickle.loads(fast_decompressed[4:])
+        return pickle.loads(fast_decompressed[3:])
     else:
-        return numpy.reshape(numpy.frombuffer(fast_decompressed), newshape=(h, w))
-
-    # 1169_airlines explodes here with RAM < ?
-    # return pickle.loads(fast_decompressed)
+        return np.reshape(np.frombuffer(fast_decompressed), newshape=(h, w))
 
 
 def pack_object(obj):
