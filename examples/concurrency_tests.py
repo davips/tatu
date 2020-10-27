d6ebe92a432695c85cb951fe56aa2f9724805f48
davips <dpsabc@gmail.com>
2020-10-27 15:03:11 -0300

    Still not working

--- examples/concurrency_tests.py
+++ examples/concurrency_tests.py
@@ -1,5 +1,20 @@
+import random
+
 from tatu.sql.mysql import MySQL
 import requests
+
+def user(username=None, password=None, email=None, base_url="http://localhost:5000"):
+    """Create a new user."""
+
+    username = username or ("username" + str(random.randint(1, 100000)))
+    password = password or ("password" + str(random.randint(1, 100000)))
+    email = email or ("email@" + str(random.randint(1, 100000)) + ".com")
+
+    url_createuser = base_url + '/api/users'
+    data_createuser = {"username": username, "password": password, "name": "Teste", "email": email}
+    response_createuser = requests.post(url_createuser, json=data_createuser)
+    print(response_createuser.text)
+    return {"username": username, "password": password, "email": email}
 
 
 # Only tatu
