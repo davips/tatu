bdcbf8dd46a5a364b2935eff1ac7679889747d64
davips <dpsabc@gmail.com>
2021-01-29 15:00:00 -0300

    Centralize all request() calls inside OkaSt

--- tatu/okast.py
+++ tatu/okast.py
@@ -20,11 +20,14 @@
 #  part of this work is a crime and is unethical regarding the effort and
 #  time spent here.
 #  Relevant employers or funding agencies will be notified accordingly.
+from typing import Union
 
-import requests
+import requests as req
 
 from garoupa.uuid import UUID
 from tatu.storageinterface import StorageInterface
+
+default_url = 'http://data.analytics.icmc.usp.br'
 
 
 def j(r):
@@ -35,99 +38,108 @@ def j(r):
 class OkaSt(StorageInterface):
     """Central remote storage"""
 
-    def __init__(self, token, alias=None, threaded=True, url="http://localhost:5000", close_when_idle=False):
+    def __init__(self, token,
+                 alias=None, threaded=True, url: Union[callable, str] = "http://localhost:5000", close_when_idle=False):
         # print("STORAGE: ", url)
-        if not isinstance(url, str):
-            self.requests = url
-            self.headers = None
-        else:
-            self.requests = requests
-            self.headers = {'Authorization': 'Bearer ' + token}
+        self.token = token
+        self.external_requests = url if callable(url) else self.request
         self.url = url
         self.alias = alias
         self.prefix = self.url if isinstance(self.url, str) else ""
-        super().__init__(threaded, timeout=6,
-                         close_when_idle=close_when_idle)  # TODO: check if threading will destroy oka
+        # TODO: check if threading will destroy oka
+        super().__init__(threaded, timeout=6, close_when_idle=close_when_idle)
 
-    def intercept_errors(self, url, method, **kwargs):
-        r = getattr(self.requests, method)(self.prefix + url, headers=self.headers, **kwargs)
-        if r.ok:
-            return r
-        raise Exception(j(r)["errors"]["json"])
-
+    def request(self, route, method, **kwargs):
+        headers = {'Authorization': 'Bearer ' + self.token} if self.token else {}
+        r = getattr(req, method)(self.url + route, headers=headers, **kwargs)
+        if r.status_code == 401:
+            print("Please login before.")
+            from tatu.auth import get_token
+            self.token = get_token(self.url)
+            self.request(route, method, **kwargs)
+        elif r.status_code == 422:
+            # msg = j(r)["errors"]["json"]
+            # print(j(r))
+            pass
+        else:
+            if r.ok:
+                return r
+            msg = j(r)["errors"]["json"]
+            raise Exception(msg)
+    
     def _uuid_(self):
         # REMINDER syncing needs to know the underlying storage of okast, because the token is not constant as an identity
-        return UUID(j(self.intercept_errors(f"/api/sync_uuid", "get"))["uuid"])
+        return UUID(j(self.request(f"/api/sync_uuid", "get"))["uuid"])
 
     def _hasdata_(self, id, include_empty):
         url = f"/api/sync?uuids={id}&cat=data&fetch=false&empty={include_empty}"
-        return j(self.intercept_errors(url, "get"))["has"]
+        return j(self.request(url, "get"))["has"]
 
     def _hasstream_(self, data):
         url = f"/api/sync?uuids={data.id}&cat=stream&fetch=false"
-        return j(self.intercept_errors(url, "get"))["has"]
+        return j(self.request(url, "get"))["has"]
 
     def _getdata_(self, id, include_empty):
         url = f"/api/sync?uuids={id}&cat=data&fetch=true&empty={include_empty}"
-        return j(self.intercept_errors(url, "get"))
+        return j(self.request(url, "get"))
 
     def _getstream_(self, data):
         url = f"/api/sync?uuids={data}&cat=stream&fetch=true"
-        return j(self.intercept_errors(url, "get"))
+        return j(self.request(url, "get"))
 
     def _hasstep_(self, id):
         url = f"/api/sync?uuids={id}&cat=step&fetch=false"
-        return j(self.intercept_errors(url, "get"))["has"]
+        return j(self.request(url, "get"))["has"]
 
     def _getstep_(self, id):
         url = f"/api/sync?uuids={id}&cat=step&fetch=true"
-        return j(self.intercept_errors(url, "get"))
+        return j(self.request(url, "get"))
 
     def _getfields_(self, id):
         url = f"/api/sync/{id}/many&cat=fields"
-        return j(self.intercept_errors(url, "get"))
+        return j(self.request(url, "get"))
 
     def _hascontent_(self, ids):
         uuids = "&".join([f"uuids={id}" for id in ids])
         url = f"/api/sync?{uuids}&cat=content&fetch=false"
-        return j(self.intercept_errors(url, "get"))["has"]
+        return j(self.request(url, "get"))["has"]
 
     def _getcontent_(self, id):
         url = f"/api/sync/{id}/content"
-        r = self.intercept_errors(url, "get")
+        r = self.request(url, "get")
         return None if r.content == b'null\n' else r.content
 
     def _lock_(self, id):
         url = f"/api/sync/{id}/lock"
-        return j(self.intercept_errors(url, "put"))["success"]
+        return j(self.request(url, "put"))["success"]
 
     def _unlock_(self, id):
         url = f"/api/sync/{id}/unlock"
-        return j(self.intercept_errors(url, "put"))["success"]
+        return j(self.request(url, "put"))["success"]
 
     def _putdata_(self, id, step, inn, stream, parent, locked, ignoredup):
         kwargs = locals().copy()
         del kwargs["self"]
         url = f"/api/sync?cat=data"
-        return j(self.intercept_errors(url, "post", json={"kwargs": kwargs}))["success"]
+        return j(self.request(url, "post", json={"kwargs": kwargs}))["success"]
 
     def _putstream_(self, rows, ignoredup):
         url = f"/api/sync/many?cat=stream&ignoredup={ignoredup}"
-        return j(self.intercept_errors(url, "post", json={"rows": rows}))["n"]
+        return j(self.request(url, "post", json={"rows": rows}))["n"]
 
     def _putfields_(self, rows, ignoredup):
         url = f"/api/sync/many?cat=fields&ignoredup={ignoredup}"
-        return j(self.intercept_errors(url, "post", json={"rows": rows}))["n"]
+        return j(self.request(url, "post", json={"rows": rows}))["n"]
 
     def _putcontent_(self, id, value, ignoredup):
         url = f"/api/sync/{id}/content?ignoredup={ignoredup}"
-        return j(self.intercept_errors(url, "post", files={'bina': value}))["success"]
+        return j(self.request(url, "post", files={'bina': value}))["success"]
 
     def _putstep_(self, id, name, path, config, dump, ignoredup):
         kwargs = locals().copy()
         del kwargs["self"]
         url = f"/api/sync?cat=step"
-        return j(self.intercept_errors(url, "post", json={"kwargs": kwargs}))["success"]
+        return j(self.request(url, "post", json={"kwargs": kwargs}))["success"]
 
     def _deldata_(self, id):
         raise Exception(f"OkaSt cannot delete Data entries! HINT: deactivate post {id} on Oka.")
